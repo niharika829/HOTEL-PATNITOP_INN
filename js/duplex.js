ab2cdf5e5b4277ab04867fa4f78a9c6b8d92ba87
niharika829 <ns1324y@gmail.com>
2020-07-09 16:49:41 +0530

    first commit

---
+++ js/duplex.js
@@ -0,0 +1,83 @@
+document.getElementById("top_chefs").disabled=true;
+
+
+ 
+ var mySwiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  loop: true,
+  pagination: ".swiper-pagination",
+  grabCursor: true,
+  speed: 5000,
+  paginationClickable: true,
+  parallax: true,
+  autoplay: true,
+  effect: "slide",
+  mousewheelControl: 1
+});
+
+
+
+/*linens*/
+document.getElementById("price").disabled=true;
+document.getElementById("left").disabled=true;
+
+
+
+
+
+
+<!--price slider-->
+$(document).ready(function() {
+          $("#slider").slider({
+              animate: true,
+              value:1,
+              min: 1,
+              max: 40,
+              step: 1,
+              slide: function(event, ui) {
+                  update(1,ui.value); //changed
+              }
+          });
+
+          $("#slider2").slider({
+              animate: true,
+              value:110,
+              min: 110,
+              max: 440,
+              step: 110,
+              slide: function(event, ui) {
+                  update(2,ui.value); //changed
+              }
+          });
+
+          //Added, set initial value.
+          $("#amount").val(0);
+          $("#duration").val(0);
+          $("#amount-label").text(0);
+          $("#duration-label").text(0);
+          
+          update();
+      });
+
+    
+      function update(slider,val) {
+
+        var $amount = slider == 1?val:$("#amount").val();
+        var $duration = slider == 2?val:$("#duration").val();
+
+     
+         $total = "$" + ($amount * $duration);
+         $( "#amount" ).val($amount);
+         $( "#amount-label" ).text($amount);
+         $( "#duration" ).val($duration);
+         $( "#duration-label" ).text($duration);
+         $( "#total" ).val($total);
+         $( "#total-label" ).text($total);
+
+         $('#slider a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$amount+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+         $('#slider2 a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$duration+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+      }
+
+
+
+
---
+++ js/vid.js
@@ -0,0 +1,120 @@
+/*vid*/
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+ const text = baffle(".data");
+    text.set({
+       
+        characters : '~!@#$%^&*()-+=[]{}|;:,./<>?+-•~!=*',
+        speed: 120
+    });
+    text.start();
+    text.reveal(4000);
+
+$(function(){
+	var	btn = $(".slider__btn");
+	
+	btn.on("click",function(){
+		$(".slider__item").first().clone().appendTo(".slider");
+		$(".slider__image").first().css({transform: "rotateX(-180deg)", opacity: 0});
+		setTimeout(function(){
+			$(".slider__item").first().remove();
+		},200);
+	});
+});
+
+
+$('.flip').hover(function(){
+        $(this).find('.card').toggleClass('flipped');
+
+ });
+   wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
+
+
+
+  
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+function save(){
+var first=document.getElementById("exampleInput").value;
+var mail=document.getElementById("exampleInputEmail1").value;
+var com=document.getElementById("exampleFormControlTextarea1").value;
+console.log(first.length);
+if(first.length=== 0){alert("please fill the name section")}
+else if(mail.length===0){alert("please fill the email section")}
+else if(com.length===0){alert("please fill the message section")}
+	else{alert("your query is successfully delivered to us we will get back to you soon");
+	
+	}
+	
+}
+
+
+
+
+
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
